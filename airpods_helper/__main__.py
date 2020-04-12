aa0af35c6c6ab26ce0924ae80969d67642a0f3b8
Konstantin Bogdanov <konstantin.bogdanov@akvelon.com>
2020-04-13 01:31:11 +0300

    Make MAC address configurable

--- airpods_helper/__main__.py
+++ airpods_helper/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import logging
 from gi.repository import GLib
 
@@ -10,7 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> int:
-    airpods_manager = AirpodsConnectionManager('14:87:6A:13:20:A2')
+    if len(sys.argv) < 2 or not sys.argv[1]:
+        logger.error('Please pass your Airpods MAC address as an argument')
+        return 1
+
+    mac_address = sys.argv[1]
+
+    airpods_manager = AirpodsConnectionManager(mac_address)
     playerctld_manager = PlayercltdPlayerManager()
 
     airpods_manager.subscribe(
